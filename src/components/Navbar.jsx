f873d1aeb67207faf2c922deb73fc501cb058f3c
duanshi-26 <duanshichawla2612@gmail.com>
2025-04-10 20:43:35 +0530

    navbar done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,16 @@
 import logo from '../assets/logo.png'
 import React from 'react'
 import {navItems} from '../constants'
+import {Menu, X} from 'lucide-react'
+import { useState } from 'react'
+
 const Navbar = () => {
+    const [mobileDrawerOpen, setMobileDrawerOpen] = useState(false);
+
+    function toggleMobileDrawer()
+    {
+        setMobileDrawerOpen(!mobileDrawerOpen);
+    }
   return (
     <nav className="sticky top-0 z-50 py-3 backdrop-blur-lg border-b border-neutral-700/80">
         <div className="container px-4 mx-auto relative text-sm">
@@ -27,10 +36,35 @@ const Navbar = () => {
                     <a href="#" className='py-2 px-3 border rounded-md'>Sign In</a>
                     <a href="#" className='bg-gradient-to-r from-orange-500 to-orange-800 py-2 px-3 rounded-md'>Create An Account</a>
                 </div>
+
+                {/* ab mobile wala toggle ka scene  */}
+                <div className="lg:hidden md:flex flex-col justify-end">
+                    <button onClick={toggleMobileDrawer}>{mobileDrawerOpen ? <X/> : <Menu/>}</button>
+                </div>
             </div>
+
+            {/* mobile size mein features dikhana */}
+            {mobileDrawerOpen && (
+                <div className="fixed right- z-20 bg-neutral-900 w-full p-12 flex flex-col justify-center items-center lg:hidden">
+                    <ul>
+                        {navItems.map((items, index)=>
+                        (
+                            <li className="py-4" key={index}>
+                                <a href={items.href}>{items.label}</a>
+                            </li>
+                        ))}
+                    </ul>
+
+                    <div className="flex space-x-6">
+                        <a href="#" className="py-2 px-3 border rounded-md">Sign In</a>
+                        <a href="#" className='py-2 px-3 rounded-md bg-gradient-to-r from-orange-500 to-orange-800'>Create An Account</a>
+                    </div>
+                </div>
+            )}
         </div>
     </nav>
   )
 }
+
 
 export default Navbar
